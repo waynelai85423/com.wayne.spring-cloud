4b732da54afc59d2458d085007e9d1f1740f8ebd
wayne <ggyy45529441@gmail.com>
2024-07-27 12:29:25 +0800

    20240727

---
+++ provide-payment/src/main/resources/archetype-resources/src/main/java/App.java
@@ -0,0 +1,13 @@
+package $com.wayne.spring-cloud;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
