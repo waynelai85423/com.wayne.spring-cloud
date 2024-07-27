fd9539ac55c71dbe1d96d656ca84e413d73c788d
wayne <ggyy45529441@gmail.com>
2024-07-28 02:58:40 +0800

    20240728

---
+++ provide-payment/src/main/java/com/wayne/payment/ProvidePaymentApplication.java
@@ -0,0 +1,13 @@
+package com.wayne.payment;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProvidePaymentApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProvidePaymentApplication.class, args);
+	}
+
+}
