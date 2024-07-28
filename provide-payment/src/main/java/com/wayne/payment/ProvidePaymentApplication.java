a384b35515c528d1cdb05b09603b64652fa5e551
wayne <ggyy45529441@gmail.com>
2024-07-28 18:59:03 +0800

    20240728-2

--- provide-payment/src/main/java/com/wayne/payment/ProvidePaymentApplication.java
+++ provide-payment/src/main/java/com/wayne/payment/ProvidePaymentApplication.java
@@ -3,7 +3,7 @@ package com.wayne.payment;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-@SpringBootApplication
+@SpringBootApplication(scanBasePackages = {"com.wayne.payment","com.wayne.utils"})
 public class ProvidePaymentApplication {
 
 	public static void main(String[] args) {
